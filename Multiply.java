959b384695a23e49d199f44900f257d9aa106d53
Kimsongbeen <ksb10266@naver.com>
2024-09-03 15:53:39 +0900

    mul

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply{
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 5;
+        int mul; 
+        mul = x * y;
+
+        System.out.println(mul);
+
+    }
+}
